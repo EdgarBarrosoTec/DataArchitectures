578c54109bb6a64f70998a6d0fd4ebb725c273cb
Angel Ricardo <angelricardo.dev@gmail.com>
2022-03-14 17:44:08 -0600

    Primera versió de arquitectura para quiosco turístico

---
+++ 2-tourist-kiosk/README.md
@@ -0,0 +1,19 @@
+# Arquitectura Quiosco Turístico
+
+# Acerca de
+
+Esta arquitectura plantea una solución para el procesamiento de datos recolectados por formularios o conversaciones de chat/voz para un quiozco turístico.
+
+La idea es que se las personas puedan interactuar con la plataforma, luego, procesar esta información con el fin de generar mejores experiencias en los centros turísticos.
+
+# Diagrama
+
+![image info](./tourist-kiosk-architecture.png)
+
+# Descripción
+
+# Costos
+
+Los costos para mantener la arquitectura se pueden obtener utilizando la calculadora de costos de [GCP].
+
+[gcp]: https://cloud.google.com/products/calculator
---
+++ 2-tourist-kiosk/diagram.py
@@ -0,0 +1,131 @@
+import sys
+import os
+sys.path.append(os.path.abspath("../shared/"))
+
+import colors
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+from diagrams.generic.blank import Blank
+from diagrams.gcp.ml import NaturalLanguageAPI, TranslationAPI, DialogFlowEnterpriseEdition
+from diagrams.gcp.api import APIGateway
+from diagrams.gcp.network import CDN
+from diagrams.gcp.storage import Storage
+from diagrams.gcp.compute import Run, Functions
+from diagrams.gcp.devtools import Scheduler
+from diagrams.gcp.database import SQL, Memorystore
+from diagrams.gcp.analytics import Bigquery
+from diagrams.onprem.analytics import Tableau, PowerBI
+from diagrams.firebase.develop import Authentication
+from diagrams.generic.device import Mobile, Tablet
+from diagrams.elastic.elasticsearch import Elasticsearch, Kibana, Logstash, Beats
+
+with Diagram(name="Arquitectura Quiosco turístico", show=False, filename="tourist-kiosk-architecture"):  
+  web = Custom("Web", "../assets/internet.png")
+
+  with Cluster("Leyenda"):
+    with Cluster("Tipo de conexión"):
+      Blank() - Edge(label="Conexión Local", color=colors.black, style="dotted") - Blank()
+      Blank() - Edge(label="Conexión Remota", color=colors.black) - Blank()
+    
+    with Cluster("Dirección del flujo"):
+      a_unidirectional, b_unidirectional = Blank(), Blank()
+      a_unidirectional >> Edge(label="Flujo unidireccional", color=colors.black) >> b_unidirectional
+      a_unidirectional << Edge(label="", color=colors.black) << b_unidirectional      
+      Blank() << Edge(label="Flujo bidireccional", color=colors.black) >> Blank()
+
+    with Cluster("Colores"):
+      Blank() - Edge(label="Proposito general", color=colors.black) - Blank()
+      Blank() - Edge(label="Tráfico web", color=colors.orange) - Blank()
+      Blank() - Edge(label="Entrada (servicios externos), Salida (Visualización)", color=colors.dark_blue) - Blank()
+      Blank() - Edge(label="Depuración, captura de errores, métricas", color=colors.light_blue) - Blank()   
+
+  with Cluster("Ingesta de datos"):
+    with Cluster("Archivos"):
+      drive = Custom("Google Drive", "../assets/google-drive.png")
+      csv = Custom("CSV", "../assets/file.png")
+
+    with Cluster("Formularios"):
+      kobotoolbox = Custom("Kobotoolbox", "../assets/kobotoolbox.png")
+      google_docs = Custom("Google Docs", "../assets/google-docs.png")
+
+  with Cluster("Visualización"):    
+    smartphone = Mobile("Smartphone")
+    tablet = Tablet("Tablet")
+    computer = Custom("Computadora", "../assets/computer.png")
+    tableau = Tableau("Tableau")
+    powerbi = PowerBI("Power BI")
+    gds = Custom("Google Data Studio", "../assets/google-data-studio.png")  
+
+  with Cluster("Google Cloud Platform"):
+    google_assistant = Custom("Google Assistant", "../assets/google-assistant.png")
+    scheduler = Scheduler("Scheduler\n(Programador de tareas)")
+    firebase_auth = Authentication("Firebase Authentication")
+    cdn = CDN("Cloud CDN\n(Distribución de contenido)")
+
+    with Cluster("Almacenamiento de datos"):
+      bigquery = Bigquery("BigQuery\n(Almacen de datos)")
+      base_datos = SQL("SQL\n(Historico, BD Core)")
+      cache = Memorystore("Memorystore\n(Cache)")
+
+    with Cluster("Backend"):
+      contenedor = Run("Cloud Run\n(Aplicación)")
+      bucket = Storage("Storage\n(Multimedia, Archivos)")
+
+    with Cluster("Procesamiento de lenguaje natural"):
+      nlp = NaturalLanguageAPI("Natural Language API\n(Análisis de lenguaje natural)")
+      nlp_in = Storage("Storage\nFormularios")
+      nlp_function = Functions("Functions\n(Rutina de procesamiento)")      
+      translations = TranslationAPI("Translation API\n(Traducción de idiomas)")
+      dialogflow = DialogFlowEnterpriseEdition("DialogFlow\n(IA de Converssación)")
+
+    with Cluster("Entrada de datos"):
+      function1 = Functions("Cloud Function")
+      api_gateway = APIGateway("API Gateway")
+
+    with Cluster("Registros y monitoreo"):
+      elasticsearch = Elasticsearch("Elasticsearch\n(Registro de eventos/errores)")      
+      logstash = Logstash("Logstash\n(Procesamiento de logs)")
+      beats = Beats("Beats\n(Ingesta)")
+      kibana = Kibana("Kibana\n(Monitorización y Debugging)")
+
+  # Conexiones GCP - Almacenamiento de datos
+  base_datos << Edge(color=colors.black, style="dotted") >> cache
+  bigquery << Edge(color=colors.black, style="dotted") >> cache
+  cache << Edge(color=colors.black, style="dotted") >> contenedor
+
+  # Conexiones GCP - Backend
+  contenedor << Edge(color=colors.black, style="dotted") >> bucket
+  firebase_auth << Edge(color=colors.black, style="dotted") >> contenedor
+  cdn << Edge(color=colors.black, style="dotted") >> firebase_auth
+
+  # Conexiones GCP - Entrada de datos
+  function1 << Edge(color=colors.black, style="dotted") << api_gateway
+  function1 >> Edge(color=colors.black, style="dotted") >> nlp_in
+  csv >> Edge(color=colors.dark_blue) >> nlp_in
+  kobotoolbox >> Edge(color=colors.dark_blue) >> api_gateway
+  google_docs >> Edg(color=colors.dark_blue) >> api_gateway
+
+  # Conexiones GCP - Visualización
+  [ smartphone, tablet, computer ] << Edge(color=colors.orange) >> web
+  web << Edge(color=colors.orange) >> cdn
+  gds << Edge(color=colors.dark_blue) >> bigquery
+  [ tableau, powerbi ] << Edge(color=colors.dark_blue) >> cache
+  smartphone >> Edge(color=colors.black) >> google_assistant
+
+  # Conexiones GCP - Registros y monitoreo
+  beats >> Edge(color=colors.black, style="dotted") >> logstash
+  logstash >> Edge(color=colors.black, style="dotted") >> elasticsearch
+  elasticsearch << Edge(color=colors.black, style="dotted") >> kibana
+  beats << Edge(color=colors.light_blue, style="dashed") << contenedor
+  beats << Edge(color=colors.light_blue, style="dashed") << function1
+  nlp_function >> Edge(color=colors.light_blue, style="dashed") >> beats
+
+  # Procesamiento de lenguaje natural
+  nlp_function << Edge(color=colors.black, style="dotted") << nlp_in
+  nlp_function >> Edge(color=colors.black, style="dotted") >> nlp  
+  scheduler >> Edge(color=colors.black, style="dotted", label="iniciar procesamiento") >> nlp_function  
+  nlp >> Edge(color=colors.black, style="dotted") >> translations
+  translations >> Edge(color=colors.black, style="dotted") >> bigquery
+  web >> Edge(color=colors.dark_blue) >> dialogflow  
+  google_assistant >> Edge(color=colors.black, style="dotted") >> dialogflow
+  dialogflow << Edge(color=colors.black, style="dotted") >> contenedor
