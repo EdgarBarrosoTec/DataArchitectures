699edec26d718c9dae93675d9761d954ea83f8ee
Angel Ricardo <angelricardo.dev@gmail.com>
2022-03-17 11:10:17 -0600

    Se agrega redes sociales a diagrama Multimedia

--- 4-multimedia/diagram.py
+++ 4-multimedia/diagram.py
@@ -36,6 +36,12 @@ with Diagram(name="Arquitectura de Datos Multimedia", graph_attr=graph_attr, sho
     gds = Custom("Google Data Studio", "../assets/google-data-studio.png")  
 
   with Cluster("Servicios Externos"):
+    with Cluster("Redes sociales"):
+      facebook = Custom("Facebook", "../assets/facebook.png")
+      instagram = Custom("Instagram", "../assets/instagram.png")
+      twitter = Custom("Twitter", "../assets/twitter.png")
+      linkedin = Custom("LinkedIn", "../assets/linkedin.png")
+
     with Cluster("Archivos"):
       drive = Custom("Google Drive", "../assets/google-drive.png")
       csv = Custom("CSV", "../assets/file.png")
@@ -59,6 +65,7 @@ with Diagram(name="Arquitectura de Datos Multimedia", graph_attr=graph_attr, sho
       image_processing = Functions("Cloud Function\n(Procesar al subir imagen)")
       image_storage = Storage("Almacenamiento de imágenes")
       store_image_metadata = Functions("Cloud Function\n(Guardar metadata de imagen)")
+      media_storage_connector = Blank("", height="0.0", width="0.0")
       Blank("")
 
     with Cluster("Procesamiento de video"):
@@ -87,8 +94,9 @@ with Diagram(name="Arquitectura de Datos Multimedia", graph_attr=graph_attr, sho
       bucket = Storage("Storage\n(Archivos Estáticos)")
 
     with Cluster("Servicios Externos"):
-      function1 = Functions("Cloud Function")
+      function1 = Functions("Cloud Function\n(Entrada)")
       api_gateway = APIGateway("API Gateway")
+      socialmedia_func = Functions("Cloud Function\n(Extract Post, Likes, Comments,\nVideo, Image, etc)")
 
     with Cluster("Registros y monitoreo"):
       elasticsearch = Elasticsearch("Elasticsearch\n(Registro de eventos/errores)")      
@@ -100,6 +108,8 @@ with Diagram(name="Arquitectura de Datos Multimedia", graph_attr=graph_attr, sho
   base_datos << Edge(color=colors.black, style="dotted") >> cache
   bigquery << Edge(color=colors.black, style="dotted") >> cache
   cache << Edge(color=colors.black, style="dotted") >> contenedor
+  [ speach_to_text, video_metadata, store_image_metadata ] - Edge(color=colors.black, style="dotted,bold", tailport="e", headport="w", minlen="1") - media_storage_connector  
+  media_storage_connector >> Edge(color=colors.black, style="dotted,bold") >> bigquery
 
   # Conexiones GCP - Backend
   contenedor << Edge(color=colors.black, style="dotted") >> bucket
@@ -122,24 +132,27 @@ with Diagram(name="Arquitectura de Datos Multimedia", graph_attr=graph_attr, sho
 
   # GCP - Servicios Externos
   api_gateway >> Edge(color=colors.black, style="dotted") >> function1
-  function1 >> Edge(color=colors.black, style="dotted") >> contenedor
+  function1 >> Edge(color=colors.black, style="dotted,bold", tailport="n", headport="n", minlen="1") >> contenedor
   kobotoolbox >> Edge(color=colors.dark_blue, tailport="e", headport="w", minlen="1") >> api_gateway
   erp >> Edge(color=colors.black) >> [ api_gateway, bigquery ]
-  csv >> Edge(color=colors.dark_blue) >> bucket
+  csv >> Edge(color=colors.dark_blue, tailport="n", headport="w", minlen="3") >> bucket
+  socialmedia_connector = Blank("", height="0.0", width="0.0")
+  [ facebook, instagram, twitter, linkedin ] - Edge(color=colors.purple, tailport="e", headport="w", minlen="1") - socialmedia_connector
+  socialmedia_connector >> Edge(color=colors.purple, tailport="e", headport="n", minlen="5") >> api_gateway
+  api_gateway >> Edge(color=colors.black, style="dotted", tailport="e", headport="w", minlen="1") >> socialmedia_func
+  socialmedia_func >> Edge(color=colors.black, style="dotted", tailport="e", headport="w", minlen="1") >> [ video_storage, image_storage, text_storage ]
 
   # GCP - Procesamiento de imágenes
-  image >> Edge(color=colors.dark_blue) >> image_storage
+  image >> Edge(color=colors.dark_blue, tailport="e", headport="w", minlen="1") >> image_storage
   image_storage >> Edge(color=colors.black, style="dotted") >> image_processing
   image_processing >> Edge(color=colors.black, style="dotted") >> vision_api
-  vision_api >> Edge(color=colors.black, style="dotted") >> store_image_metadata
-  store_image_metadata >> Edge(color=colors.black, style="dotted") >> bigquery
+  vision_api >> Edge(color=colors.black, style="dotted") >> store_image_metadata  
 
   # GCP - Procesamiento de video
   video >> Edge(color=colors.dark_blue) >> video_storage
   video_storage >> Edge(color=colors.black, style="dotted") >> video_processing
   video_processing >> Edge(color=colors.black, style="dotted") >> video_intelligence_api
-  video_intelligence_api >> Edge(color=colors.black, style="dotted") >> video_metadata
-  video_metadata >> Edge(color=colors.black, style="dotted") >> bigquery
+  video_intelligence_api >> Edge(color=colors.black, style="dotted") >> video_metadata  
 
   # GCP - Procesamiento de audio
   audio >> Edge(color=colors.dark_blue) >> audio_storage
@@ -147,7 +160,6 @@ with Diagram(name="Arquitectura de Datos Multimedia", graph_attr=graph_attr, sho
   audio_storage >> Edge(color=colors.black, style="dotted") >> audio_speech_processing
   text_storage >> Edge(color=colors.black, style="dotted") >> text_processing
   audio_speech_processing >> Edge(color=colors.black, style="dotted") >> speach_to_text
-  speach_to_text >> Edge(color=colors.black, style="dotted") >> translation
-  speach_to_text >> Edge(color=colors.black, style="dotted") >> bigquery
+  speach_to_text >> Edge(color=colors.black, style="dotted") >> translation  
   text_processing >> Edge(color=colors.black, style="dotted") >> text_to_speach
   text_to_speach >> Edge(color=colors.black, style="dotted") >> translation
